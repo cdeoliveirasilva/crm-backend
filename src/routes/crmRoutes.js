b0fb5a67758d6aa5a5defd84bdb4d5d6f39a5744
carla061994 <ca.oliveirasilva@hotmail.com>
2020-07-30 23:55:03 +0200

    Created GET endpoint

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,4 +1,24 @@
-import { addNewContact } from "../controlers/crmControler";
+import { addNewContact, getContacts } from "../controlers/crmControler";
+
+const routes = (app) => {
+  app
+    .route("/contact")
+    .get(getContacts)
+
+    .post(addNewContact);
+
+  app
+    .route("/contact/:contactID")
+    .put((req, res) => res.send("PUT request succesful!")) //updates specific ID
+
+    .delete((req, res) => res.send("DELETE request succesful!"));
+};
+
+export default routes;
+
+/** (How did you get here again?)
+
+import { addNewContact, getContacts } from "../controlers/crmControler";
 
 const routes = (app) => {
   app
@@ -16,12 +36,5 @@ const routes = (app) => {
     )
 
     .post(addNewContact);
-
-  app
-    .route("/contact/:contactID")
-    .put((req, res) => res.send("PUT request succesful!")) //updates specific ID
-
-    .delete((req, res) => res.send("DELETE request succesful!"));
-};
-
-export default routes;
+(...)
+ */
